9c2f061b6212a5720539b0e2e87082f16e8de8bf
kelson-cosme <kelson.almeida123@gmail.com>
2025-03-28 08:15:00 -0400

    page contato

--- src/components/pages/contato/Contato.tsx
+++ src/components/pages/contato/Contato.tsx
@@ -1,7 +1,87 @@
+import {Card} from "@/components/ui/card"
+
+import Footer from "@/components/footer/Footer"
+import { Link } from "react-router-dom"
+import { motion } from "framer-motion";
+
 function Contato(){
     return(
-        <>
-            Pagina contato
+        <>  
+            <section className="max-w-[1366px] m-auto pl-5 pr-5 pb-5 sm:pt-5">
+      <motion.h1
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5 }}
+        className="text-5xl font-medium"
+      >
+        Vamos nos conectar!
+      </motion.h1>
+
+      <motion.p
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.2 }}
+      >
+        Seja fazendo um brainstorming de um novo projeto, discutindo otimizações de design ou apenas compartilhando insights criativos, estou sempre animado para colaborar. Entre em contato comigo e vamos criar algo excepcional — juntos!
+      </motion.p>
+
+      <motion.div
+        initial={{ opacity: 0, y: 20 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ duration: 0.5, delay: 0.4 }}
+        className="p-5 mt-6 flex-col grid grid-cols-2"
+      >
+        <motion.div whileHover={{ scale: 1.05 }}>
+          <Link 
+            to={"https://wa.me/5565996646289"} 
+            target="_blank" 
+            className="flex items-center"
+          >
+            <div className="w-[15%]">
+              <img 
+                className="w-[100%]" 
+                src="https://ijvswpmlxhukauulpzjd.supabase.co/storage/v1/object/public/imagens/redeSociais/zap.webp" 
+                alt="WhatsApp" 
+              />
+            </div>
+            <p className="p-3">(65) 99664-6289</p>
+          </Link>
+        </motion.div>
+
+        <motion.div whileHover={{ scale: 1.05 }}>
+          <Link 
+            className="flex items-center" 
+            to={"https://google.com"} 
+            target="_blank"
+          >
+            <div className="w-[15%]">
+              <img 
+                src="https://ijvswpmlxhukauulpzjd.supabase.co/storage/v1/object/public/imagens/redeSociais/linkedin.webp" 
+                alt="LinkedIn" 
+              />
+            </div>
+            <p className="p-3">kelson-cosme</p>
+          </Link>
+        </motion.div>
+
+        <motion.div whileHover={{ scale: 1.05 }}>
+          <Link 
+            to={"/"} 
+            className="flex items-center"
+          >
+            <div className="w-[15%]">
+              <img 
+                src="https://ijvswpmlxhukauulpzjd.supabase.co/storage/v1/object/public/imagens/redeSociais/insta.webp" 
+                alt="Instagram" 
+              />
+            </div>
+            <p className="p-3">kelson.almeida</p>
+          </Link>
+        </motion.div>
+      </motion.div>
+    </section>
+
+            <Footer/>
         </>
     )
 }
--- src/components/pages/stack/Stack.tsx
+++ src/components/pages/stack/Stack.tsx
@@ -3,16 +3,16 @@ import Footer from "@/components/footer/Footer"
 
 function Stack(){
     return(
-        <section className="max-w-[1366px] m-auto p-5">
+        <section className="max-w-[1366px] m-auto pl-5 pr-5 pb-5 sm:pt-5">
             <h1 className="text-5xl font-medium">Stacks</h1>
             <p className="mt-4">
                 Hoje em dia trabalho com varias ferramentas para dar vida às minhas ideias. Meu objetivo é criar designs que não sejam apenas visualmente atraentes, mas também otimizados para usabilidade, acessibilidade e desempenho.
             </p>
 
             <Card className="p-5 mt-6 flex-col">
-                <h2 className="font-bold">Ferramentas de Design e Prototipagem</h2>
+                <h2 className="text-[17px] font-bold">🖌️ Ferramentas de Design e Prototipagem</h2>
 
-                <div className="grid sm:grid-cols-2 gap-5">
+                <div className="grid sm:grid-cols-2 gap-9">
 
                     <div>
                         <div  className="w-40 flex items-center">
@@ -20,7 +20,7 @@ function Stack(){
                             <h3 className="font-medium">Fima</h3>
                         </div>
 
-                        <p>Eu uso o Figma para criar wireframes, protótipos e designs de UI de alta fidelidade. Seus recursos de colaboração em tempo real o tornam perfeito para trabalhar em estreita colaboração com as equipes e obter feedback instantâneo.</p>
+                        <p className="text-justify">Eu uso o Figma para criar wireframes, protótipos e designs de UI de alta fidelidade. Seus recursos de colaboração em tempo real o tornam perfeito para trabalhar em estreita colaboração com as equipes e obter feedback instantâneo.</p>
                     </div>
 
                     <div>
@@ -29,15 +29,15 @@ function Stack(){
                             <h3 className="font-medium">Photoshop</h3>
                         </div>
 
-                        <p>
+                        <p className="text-justify">
                             O Photoshop é essencial para mim ao editar imagens ou criar recursos visuais detalhados, como ícones ou banners que aprimoram a interface do usuário.
                         </p>
                     </div>
                 </div>
 
-                <h2 className="font-bold">Ferramentas de Desenvolvimento e Implementação</h2>
+                <h2 className="text-[17px] font-bold">🛠️ Ferramentas de Desenvolvimento e Implementação</h2>
                 
-                <div className="grid sm:grid-cols-2 gap-5">
+                <div className="grid sm:grid-cols-2 gap-9">
 
                     <div>
                         <div  className="w-40 flex items-center">
@@ -45,7 +45,7 @@ function Stack(){
                             <h3 className="font-medium">HTML</h3>
                         </div>
 
-                        <p>Eu uso o Figma para criar wireframes, protótipos e designs de UI de alta fidelidade. Seus recursos de colaboração em tempo real o tornam perfeito para trabalhar em estreita colaboração com as equipes e obter feedback instantâneo.</p>
+                        <p className="text-justify">Utilizo HTML para estruturar o conteúdo das páginas web, garantindo que todos os elementos da interface sejam semanticamente organizados e acessíveis.</p>
                     </div>
 
                     <div>
@@ -54,9 +54,8 @@ function Stack(){
                             <h3 className="font-medium">CSS</h3>
                         </div>
 
-                        <p>
-                            O Photoshop é essencial para mim ao editar imagens ou criar recursos visuais detalhados, como ícones ou banners que aprimoram a interface do usuário.
-                        </p>
+                        <p className="text-justify">
+                        Uso CSS para estilizar e dar vida às interfaces que desenvolvo, criando layouts responsivos, animações sutis e experiências visuais agradáveis para o usuário.</p>
                     </div>
 
                     <div>
@@ -65,9 +64,8 @@ function Stack(){
                             <h3 className="font-medium">ReactJs</h3>
                         </div>
 
-                        <p>
-                            O Photoshop é essencial para mim ao editar imagens ou criar recursos visuais detalhados, como ícones ou banners que aprimoram a interface do usuário.
-                        </p>
+                        <p className="text-justify">
+                        Emprego ReactJS para construir interfaces dinâmicas e interativas, utilizando componentes reutilizáveis que facilitam a escalabilidade e manutenção dos projetos.</p>
                     </div>
 
                     <div>
@@ -76,9 +74,8 @@ function Stack(){
                             <h3 className="font-medium">NodeJs</h3>
                         </div>
 
-                        <p>
-                            O Photoshop é essencial para mim ao editar imagens ou criar recursos visuais detalhados, como ícones ou banners que aprimoram a interface do usuário.
-                        </p>
+                        <p className="text-justify">
+                        Utilizo NodeJS para desenvolver o backend das aplicações, criando APIs eficientes e garantindo uma comunicação rápida e segura entre front-end e banco de dados.</p>
                     </div>
 
                     <div>
@@ -87,9 +84,8 @@ function Stack(){
                             <h3 className="font-medium">Tailwind</h3>
                         </div>
 
-                        <p>
-                            O Photoshop é essencial para mim ao editar imagens ou criar recursos visuais detalhados, como ícones ou banners que aprimoram a interface do usuário.
-                        </p>
+                        <p className="text-justify">
+                        Uso Tailwind CSS para estilizar interfaces de forma rápida e eficiente, aplicando classes utilitárias que tornam o desenvolvimento mais ágil e organizado.</p>
                     </div>
                     
                     <div>
@@ -98,9 +94,8 @@ function Stack(){
                             <h3 className="font-medium">Vite</h3>
                         </div>
 
-                        <p>
-                            O Photoshop é essencial para mim ao editar imagens ou criar recursos visuais detalhados, como ícones ou banners que aprimoram a interface do usuário.
-                        </p>
+                        <p className="text-justify">
+                        O Vite é minha ferramenta preferida para criar projetos front-end modernos. Ele oferece um ambiente de desenvolvimento super rápido, com recarregamento instantâneo e build otimizado.</p>
                     </div>
 
                     <div>
@@ -109,9 +104,8 @@ function Stack(){
                             <h3 className="font-medium">TypeScript</h3>
                         </div>
 
-                        <p>
-                            O Photoshop é essencial para mim ao editar imagens ou criar recursos visuais detalhados, como ícones ou banners que aprimoram a interface do usuário.
-                        </p>
+                        <p className="text-justify">
+                        Utilizo TypeScript para adicionar tipagem estática ao JavaScript, aumentando a segurança do código e facilitando a identificação de erros durante o desenvolvimento.</p>
                     </div>
 
                     <div>
@@ -120,9 +114,8 @@ function Stack(){
                             <h3 className="font-medium">JavaScript</h3>
                         </div>
 
-                        <p>
-                            O Photoshop é essencial para mim ao editar imagens ou criar recursos visuais detalhados, como ícones ou banners que aprimoram a interface do usuário.
-                        </p>
+                        <p className="text-justify">
+                        JavaScript é essencial para mim na criação de interatividade e dinamismo nas interfaces web, possibilitando a construção de experiências fluidas para o usuário.</p>
                     </div>
 
                     
@@ -132,9 +125,8 @@ function Stack(){
                             <h3 className="font-medium">Shadcn UI/UX</h3>
                         </div>
 
-                        <p>
-                            O Photoshop é essencial para mim ao editar imagens ou criar recursos visuais detalhados, como ícones ou banners que aprimoram a interface do usuário.
-                        </p>
+                        <p className="text-justify">
+                        Utilizo o Shadcn UI para acelerar a criação de componentes visuais elegantes e acessíveis, garantindo uma interface limpa e consistente.</p>
                     </div>
 
                     <div>
@@ -143,15 +135,14 @@ function Stack(){
                             <h3 className="font-medium">Api MercadoPago</h3>
                         </div>
 
-                        <p>
-                            O Photoshop é essencial para mim ao editar imagens ou criar recursos visuais detalhados, como ícones ou banners que aprimoram a interface do usuário.
-                        </p>
+                        <p className="text-justify">
+                        Uso a API do Mercado Pago para integrar sistemas de pagamento nas aplicações, garantindo uma experiência de compra segura e fluida para o usuário.</p>
                     </div>
 
                 </div>
 
 
-                <h2 className="font-bold">Banco de Dados</h2>
+                <h2 className="text-[17px] font-bold">🗄️ Banco de Dados</h2>
 
                 <div className="grid sm:grid-cols-2 gap-5">
 
@@ -161,7 +152,7 @@ function Stack(){
                             <h3 className="font-medium">Firebase</h3>
                         </div>
 
-                        <p>Eu uso o Figma para criar wireframes, protótipos e designs de UI de alta fidelidade. Seus recursos de colaboração em tempo real o tornam perfeito para trabalhar em estreita colaboração com as equipes e obter feedback instantâneo.</p>
+                        <p className="text-justify">Utilizo o Firebase para gerenciar autenticação, banco de dados em tempo real e hospedagem de aplicações, oferecendo uma solução completa para projetos web e mobile.</p>
                     </div>
 
                     <div>
@@ -170,15 +161,15 @@ function Stack(){
                             <h3 className="font-medium">Supabase</h3>
                         </div>
 
-                        <p>
-                            O Photoshop é essencial para mim ao editar imagens ou criar recursos visuais detalhados, como ícones ou banners que aprimoram a interface do usuário.
+                        <p className="text-justify">
+                            Uso o Supabase como alternativa open source ao Firebase, principalmente para banco de dados PostgreSQL e autenticação simples, garantindo escalabilidade e flexibilidade nos projetos.
                         </p>
                     </div>
                 </div>
 
-                <h2 className="font-bold">Hospedagem de código-fonte</h2>
+                <h2 className="text-[17px] font-bold">🌐 Hospedagem de Código-Fonte</h2>
 
-                <div className="grid sm:grid-cols-2 gap-5">
+                <div className="grid sm:grid-cols-2 gap-9">
 
                     <div>
                         <div  className="w-40 flex items-center">
@@ -186,7 +177,7 @@ function Stack(){
                             <h3 className="font-medium">GitHub</h3>
                         </div>
 
-                        <p>Eu uso o Figma para criar wireframes, protótipos e designs de UI de alta fidelidade. Seus recursos de colaboração em tempo real o tornam perfeito para trabalhar em estreita colaboração com as equipes e obter feedback instantâneo.</p>
+                        <p className="text-justify">Utilizo o GitHub para versionar e armazenar meus projetos, facilitando a colaboração com outros desenvolvedores e mantendo um histórico organizado das alterações no código.</p>
                     </div>
                 </div>
             </Card>
