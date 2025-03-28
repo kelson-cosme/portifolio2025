2c2939dd38e245a2392fe081d7c714921397225f
kelson-cosme <kelson.almeida123@gmail.com>
2025-03-28 08:19:40 -0400

    css

--- src/components/pages/contato/Contato.tsx
+++ src/components/pages/contato/Contato.tsx
@@ -7,78 +7,81 @@ import { motion } from "framer-motion";
 function Contato(){
     return(
         <>  
-            <section className="max-w-[1366px] m-auto pl-5 pr-5 pb-5 sm:pt-5">
-      <motion.h1
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5 }}
-        className="text-5xl font-medium"
-      >
-        Vamos nos conectar!
-      </motion.h1>
-
-      <motion.p
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.2 }}
-      >
-        Seja fazendo um brainstorming de um novo projeto, discutindo otimizações de design ou apenas compartilhando insights criativos, estou sempre animado para colaborar. Entre em contato comigo e vamos criar algo excepcional — juntos!
-      </motion.p>
-
-      <motion.div
-        initial={{ opacity: 0, y: 20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ duration: 0.5, delay: 0.4 }}
-        className="p-5 mt-6 flex-col grid grid-cols-2"
-      >
-        <motion.div whileHover={{ scale: 1.05 }}>
-          <Link 
-            to={"https://wa.me/5565996646289"} 
-            target="_blank" 
-            className="flex items-center"
+        <section className="max-w-[1366px] m-auto pl-5 pr-5 pb-5 sm:pt-5">
+          
+        <motion.h1
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5 }}
+            className="text-5xl font-medium"
           >
-            <div className="w-[15%]">
-              <img 
-                className="w-[100%]" 
-                src="https://ijvswpmlxhukauulpzjd.supabase.co/storage/v1/object/public/imagens/redeSociais/zap.webp" 
-                alt="WhatsApp" 
-              />
-            </div>
-            <p className="p-3">(65) 99664-6289</p>
-          </Link>
-        </motion.div>
+            Vamos nos conectar!
+          </motion.h1>
 
-        <motion.div whileHover={{ scale: 1.05 }}>
-          <Link 
-            className="flex items-center" 
-            to={"https://google.com"} 
-            target="_blank"
+          <motion.p
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.2 }}
           >
-            <div className="w-[15%]">
-              <img 
-                src="https://ijvswpmlxhukauulpzjd.supabase.co/storage/v1/object/public/imagens/redeSociais/linkedin.webp" 
-                alt="LinkedIn" 
-              />
-            </div>
-            <p className="p-3">kelson-cosme</p>
-          </Link>
-        </motion.div>
+            Seja fazendo um brainstorming de um novo projeto, discutindo otimizações de design ou apenas compartilhando insights criativos, estou sempre animado para colaborar. Entre em contato comigo e vamos criar algo excepcional — juntos!
+          </motion.p>
 
-        <motion.div whileHover={{ scale: 1.05 }}>
-          <Link 
-            to={"/"} 
-            className="flex items-center"
+        <Card className="p-5 mt-6 flex-col">
+          <motion.div
+            initial={{ opacity: 0, y: 20 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ duration: 0.5, delay: 0.4 }}
+            className="  flex-col grid grid-cols-2"
           >
-            <div className="w-[15%]">
-              <img 
-                src="https://ijvswpmlxhukauulpzjd.supabase.co/storage/v1/object/public/imagens/redeSociais/insta.webp" 
-                alt="Instagram" 
-              />
-            </div>
-            <p className="p-3">kelson.almeida</p>
-          </Link>
-        </motion.div>
-      </motion.div>
+            <motion.div whileHover={{ scale: 1.05 }}>
+              <Link 
+                to={"https://wa.me/5565996646289"} 
+                target="_blank" 
+                className="flex items-center"
+              >
+                <div className="w-[15%]">
+                  <img 
+                    className="w-[100%]" 
+                    src="https://ijvswpmlxhukauulpzjd.supabase.co/storage/v1/object/public/imagens/redeSociais/zap.webp" 
+                    alt="WhatsApp" 
+                  />
+                </div>
+                <p className="p-3">(65) 99664-6289</p>
+              </Link>
+            </motion.div>
+
+            <motion.div whileHover={{ scale: 1.05 }}>
+              <Link 
+                className="flex items-center" 
+                to={"https://google.com"} 
+                target="_blank"
+              >
+                <div className="w-[15%]">
+                  <img 
+                    src="https://ijvswpmlxhukauulpzjd.supabase.co/storage/v1/object/public/imagens/redeSociais/linkedin.webp" 
+                    alt="LinkedIn" 
+                  />
+                </div>
+                <p className="p-3">kelson-cosme</p>
+              </Link>
+            </motion.div>
+
+            <motion.div whileHover={{ scale: 1.05 }}>
+              <Link 
+                to={"/"} 
+                className="flex items-center"
+              >
+                <div className="w-[15%]">
+                  <img 
+                    src="https://ijvswpmlxhukauulpzjd.supabase.co/storage/v1/object/public/imagens/redeSociais/insta.webp" 
+                    alt="Instagram" 
+                  />
+                </div>
+                <p className="p-3">kelson.almeida</p>
+              </Link>
+            </motion.div>
+          </motion.div>
+          </Card>
     </section>
 
             <Footer/>
